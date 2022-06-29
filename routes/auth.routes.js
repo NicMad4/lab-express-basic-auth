3018fefcc9753ad4f6e4d786d9dffad33716f2b6
Nico M <nico_16bcn@hotmail.com>
2022-06-30 00:53:48 +0200

    iteracion 2
    logIn,stay in session , log out

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,6 +9,7 @@ const saltRounds = 10;
 const User = require("../models/User.model");
 const mongoose = require('mongoose');
 
+//Aqui empiezo el  SignUp
 // GET route ==> to display the signup form to users
 router.get("/signup", (req, res) => res.render("auth/signup"));
 
@@ -63,7 +64,52 @@ router.post("/signup", (req, res, next) => {
       } else {
           next(error);
       }
-    }) // close .catch()
-}) // close router.post()
+    }) 
+}) 
+
+//  AQUI empiezo el L O G I N 
+ 
+// GET route ==> to display the login form to users
+router.get('/login', (req, res) => res.render('auth/login'));
+router.post('/login', (req, res, next) => {
+  console.log('SESSION =====> ', req.session);
+  const { email, password } = req.body;
+ 
+  if (email === '' || password === '') {
+    res.render('auth/login', {/*si esta vacio el campo email y/o pasword renderiza mensaje de error*/ 
+      errorMessage: 'Please enter both, email and password to login.'
+    });
+    return;
+  }
+ 
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {/*si es diferente a un usuario (que no existe vaya) renderiza mensaje de error*/
+        res.render('auth/login', { errorMessage: 'Email is not registered. Try with other email.' });
+        return;
+      } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        res.render('users/user-profile', { user });/*si la contraseña que te dan y la almacenada coinciden renderiza(ve a) users/user-profile  */
+      //******* Aqui el Usuario deberia quedar conectado en la sesion ********//
+      req.session.currentUser = user;
+      res.redirect('/userProfile');
+
+      } else {
+        res.render('auth/login', { errorMessage: 'Incorrect password.'}); /*si la contraseña que te dan y la almacenada  NO coinciden renderiza mensaje de error*/ 
+      }
+    })
+    .catch(error => next(error));
+});
+
+/*Para desconectarse usamos el metodo destroy */
+router.post('/logout', (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) next(err);
+    res.redirect('/');
+  });
+});
+
+router.get('/userProfile', (req, res) => { /*renderiza el perfil del usuario(usuario en sesion) */
+  res.render('users/user-profile', { userInSession: req.session.currentUser });
+});
 
 module.exports = router;
